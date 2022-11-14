980eb21d22917a4c8badd3c5d2826026fb1ae7c5
Thierry Araujo <thierryaraujo309@gmail.com>
2022-11-13 22:44:47 -0300

    login and senha invalid notification

--- src/main/java/br/edu/uerr/loja/controle/LoginController.java
+++ src/main/java/br/edu/uerr/loja/controle/LoginController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.uerr.loja.modelo.Usuario;
 
@@ -16,20 +17,18 @@ public class LoginController{
     }
 
     @PostMapping ("efetuarLogin")
-    public String efetuarLogin(Usuario usuario, HttpSession session ){
+    public String efetuarLogin(Usuario usuario,
+        RedirectAttributes ra ,
+         HttpSession session ){
         if (usuario.getLogin().equals("admin") &&
             usuario.getSenha().equals("1234"))  {
             usuario.setNome("Administrador");
              session.setAttribute("usuarioLogado", usuario);
                 return "redirect:/empresa";
-            } else {
+            } else { 
+                ra.addFlashAttribute("mensagem","login e/ou senha inválidos");
                 return"redirect:/";
             }
-
-   
-
-
-
         }
         @PostMapping("/logout")
         public String logout(HttpSession session){
